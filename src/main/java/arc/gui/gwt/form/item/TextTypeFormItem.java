df9a21710c3c55208e32e37f8b201d0ff4538990
Wilson Liu <wliu5@unimelb.edu.au>
2017-11-15 18:58:21 +1100

    4.6.022 original

--- src/main/java/arc/gui/gwt/form/item/TextTypeFormItem.java
+++ src/main/java/arc/gui/gwt/form/item/TextTypeFormItem.java
@@ -25,76 +25,75 @@ import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.user.client.ui.Widget;
 
-@SuppressWarnings("rawtypes")
-public class TextTypeFormItem implements FormItem { // NO_UCD (unused code)
 
+public class TextTypeFormItem implements FormItem {
+    
     public static enum Presentation {
-        SCROLLING, AUTO_RESIZE;
+        SCROLLING,
+        AUTO_RESIZE;
     }
-
+    
     private static Presentation _defaultPresentation = Presentation.SCROLLING;
-
+    
     public static Presentation defaultPresentation() {
         return _defaultPresentation;
     }
-
+    
     public static void setDefaultPresentation(Presentation p) {
         _defaultPresentation = p;
     }
-
-    public Widget create(Form form, final Field f, FormItemFocusListener fl, FormSubmitOnEnter fse) {
-        final TextType type = (TextType) f.definition().type();
-
-        return createWidgetFor(form, f, fl, type.minLength(), type.maxLength(), 60);
+    
+    public Widget create(Form form,final Field f,FormItemFocusListener fl,FormSubmitOnEnter fse) {
+        final TextType type = (TextType)f.definition().type();
+        
+        return createWidgetFor(form,f,fl,type.minLength(),type.maxLength(),60);
     }
-
-    private static Boolean useAutoResizeTextWidget(final Field f) {
+    
+    private static Boolean useAutoResizeTextWidget( final Field f ) {
         FieldRenderOptions ro = f.renderOptions();
-        if (ro != null) {
+        if ( ro != null ) {
             try {
-                if (ro.hasValue(TextFieldRenderOptions.AUTO_RESIZE)) {
-                    return ro.booleanValue(TextFieldRenderOptions.AUTO_RESIZE, false);
+                if ( ro.hasValue(TextFieldRenderOptions.AUTO_RESIZE) ) {
+                    return ro.booleanValue( TextFieldRenderOptions.AUTO_RESIZE, false);
                 }
-
-            } catch (Throwable t) {
+                
+            } catch ( Throwable t ) {
                 ThrowableUtil.rethrowAsUnchecked(t);
                 return false;
             }
         }
-
-        switch (_defaultPresentation) {
-        case AUTO_RESIZE:
-            return true;
-        default:
-            return false;
+        
+        switch ( _defaultPresentation ) {
+            case AUTO_RESIZE:
+                return true;
+            default:
+                return false;
         }
     }
-
-    protected static Widget createWidgetFor(final Form form, final Field f, final FormItemFocusListener fl,
-            final int minLength, final int maxLength, int height) {
-        if (useAutoResizeTextWidget(f)) {
+    
+    protected static Widget createWidgetFor(final Form form,final Field f,final FormItemFocusListener fl,final int minLength,final int maxLength,int height) {
+        if ( useAutoResizeTextWidget( f) ) {
             return createAutoResizeWidgetFor(form, f, fl, minLength, maxLength, height);
         }
-
+        
         return createDefaultWidgetFor(form, f, fl, minLength, maxLength, height);
     }
-
-    private static Widget createDefaultWidgetFor(final Form form, final Field f, final FormItemFocusListener fl,
-            final int minLength, final int maxLength, int height) {
+    
+    private static Widget createDefaultWidgetFor(final Form form,final Field f,final FormItemFocusListener fl,final int minLength,final int maxLength,int height) {
         final TextArea text = new TextArea() {
             public void onAttach() {
                 super.onAttach();
 
-                if (f.focus()) {
+                if ( f.focus() ) {
                     super.setFocus(true);
                 }
             }
-
+            
         };
-
+        
         FieldRenderOptions ro = f.renderOptions();
-        if (ro == null || ro.width() == null) {
-            if (form.isWidth100()) {
+        if ( ro == null || ro.width() == null ) {
+            if ( form.isWidth100() ) {
                 text.setPreferredWidth(NormalizedDimension.ONE);
             } else {
                 text.setWidth(200);
@@ -105,45 +104,44 @@ public class TextTypeFormItem implements FormItem { // NO_UCD (unused code)
             if (roWidth instanceof NormalizedDimension) {
                 text.setPreferredWidth(roWidth);
             } else {
-                text.setWidth((int) roWidth.transform(1));
-                text.setPreferredWidth((int) roWidth.transform(1));
+                text.setWidth((int)roWidth.transform(1));
+                text.setPreferredWidth((int)roWidth.transform(1));
             }
         }
-
-        if (ro == null || ro.height() == null) {
+        
+        if ( ro == null || ro.height() == null ) {
             text.setHeight(height);
         } else {
             Dimension roHeight = ro.height();
-            if (roHeight instanceof NormalizedDimension) {
-                text.setPreferredHeight(ro.height());
+            if ( roHeight instanceof NormalizedDimension ) {
+                text.setPreferredHeight(ro.height());   
             } else {
-                text.setHeight((int) roHeight.transform(1));
-                text.setPreferredHeight((int) roHeight.transform(1));
+                text.setHeight((int)roHeight.transform(1));
+                text.setPreferredHeight((int)roHeight.transform(1));
             }
         }
-
+        
         performCommonInitialization(text, form, f, fl, minLength, maxLength, height);
 
         return text;
 
     }
 
-    private static Widget createAutoResizeWidgetFor(final Form form, final Field f, final FormItemFocusListener fl,
-            final int minLength, final int maxLength, int height) {
+    private static Widget createAutoResizeWidgetFor(final Form form,final Field f,final FormItemFocusListener fl,final int minLength,final int maxLength,int height) {
         final AutoResizeTextArea text = new AutoResizeTextArea() {
             public void onAttach() {
                 super.onAttach();
 
-                if (f.focus()) {
+                if ( f.focus() ) {
                     super.setFocus(true);
                 }
             }
-
+            
         };
-
+        
         FieldRenderOptions ro = f.renderOptions();
-        if (ro == null || ro.width() == null) {
-            if (form.isWidth100()) {
+        if ( ro == null || ro.width() == null ) {
+            if ( form.isWidth100() ) {
                 text.setPreferredWidth(NormalizedDimension.ONE);
             } else {
                 text.setMinWidth(200);
@@ -151,33 +149,33 @@ public class TextTypeFormItem implements FormItem { // NO_UCD (unused code)
         } else {
             text.setPreferredWidth(ro.width());
         }
-
-        if (ro == null || ro.height() == null) {
+        
+        if ( ro == null || ro.height() == null ) {
             text.setMinAutoHeight(height);
         } else {
             text.setPreferredHeight(ro.height());
         }
-
+        
         performCommonInitialization(text, form, f, fl, minLength, maxLength, height);
-
+        
         return text;
 
     }
-
-    private static void performCommonInitialization(final TextWidget<com.google.gwt.user.client.ui.TextArea> text,
-            final Form form, final Field f, final FormItemFocusListener fl, final int minLength, final int maxLength,
-            int height) {
-
+    
+    private static void performCommonInitialization( 
+            final TextWidget<com.google.gwt.user.client.ui.TextArea> text,
+            final Form form,final Field f,final FormItemFocusListener fl,final int minLength,final int maxLength,int height ) {
+        
         FormItemStyle.applyReadWriteTo(text);
-
-        if (fl != null) {
+        
+        if ( fl != null ) {
             text.addFocusHandler(new FocusHandler() {
                 @Override
                 public void onFocus(FocusEvent event) {
                     fl.focusOn(f);
                 }
             });
-
+            
             text.addBlurHandler(new BlurHandler() {
                 @Override
                 public void onBlur(BlurEvent event) {
@@ -185,47 +183,46 @@ public class TextTypeFormItem implements FormItem { // NO_UCD (unused code)
                 }
             });
         }
+        
 
         text.setEnabled(f.enabled());
         text.setVisible(f.visible());
 
-        if (f.value() != null) {
+        if ( f.value() != null ) {
             text.setValue(f.value().toString());
         }
-
-        if (maxLength < Integer.MAX_VALUE) {
+        
+        if ( maxLength < Integer.MAX_VALUE ) {
             text.addKeyPressFilter(new KeyPressFilter() {
                 public boolean allow(String value, char c) {
-                    if (value == null) {
+                    if ( value == null ) {
                         return true;
                     }
-
-                    if (value.length() >= maxLength) {
+                    
+                    if ( value.length() >= maxLength ) {
                         return false;
                     }
-
+                    
                     return true;
                 }
             });
         }
 
         text.addValueChangeHandler(new ValueChangeHandler<String>() {
-            @SuppressWarnings("unchecked")
             public void onValueChange(ValueChangeEvent<String> event) {
                 String v = event.getValue();
-
-                if (v == null || v.equals("")) {
+                
+                if ( v == null || v.equals("") ) {
                     f.setValue(null);
                 } else {
                     f.setValue(v);
-                    if (v.length() < minLength) {
-                        f.markInvalid("The minimum length must be " + minLength + " character(s). The value is "
-                                + v.length() + " character(s).");
+                    if ( v.length() < minLength ) {
+                        f.markInvalid("The minimum length must be " + minLength + " character(s). The value is " + v.length() + " character(s).");
                     }
                 }
             }
         });
-
+        
         text.addResizeListener(new ResizeListener() {
             @Override
             public Widget widget() {
@@ -236,29 +233,28 @@ public class TextTypeFormItem implements FormItem { // NO_UCD (unused code)
             public void resized(long w, long h) {
                 form.notifyOfFieldResize();
             }
-
+            
         });
-
+        
         text.enableValueMonitoring();
-
-        form.addRenderListener(f, new FormItemListener() {
-            @SuppressWarnings("incomplete-switch")
-            public void itemPropertyChanged(arc.gui.form.FormItem f, arc.gui.form.FormItem.Property property) {
-
-                switch (property) {
+        
+        form.addRenderListener(f,new FormItemListener() {
+            public void itemPropertyChanged(arc.gui.form.FormItem f,arc.gui.form.FormItem.Property property) {
+                
+                switch ( property ) {
                 case VISIBILITY:
                     text.setVisible(f.visible());
                     break;
-
+                    
                 case ENABLED:
                     text.setEnabled(f.enabled());
                     FormItemFactory.updateStyle(text, f);
                     break;
-
+                    
                 case FOCUS:
                     text.setFocus(f.focus());
                     break;
-
+                    
                 case SELECT:
                     text.selectAll();
                     break;
@@ -266,59 +262,54 @@ public class TextTypeFormItem implements FormItem { // NO_UCD (unused code)
             }
 
             public void itemValueChanged(arc.gui.form.FormItem f) {
-                if (!ObjectUtil.equals(f.value(), text.value())) {
-                    if (!text.monitoredValueChanged()) {
-                        text.setValue(f.valueAsString(), false);
+                if ( !ObjectUtil.equals(f.value(),text.value()) ) {
+                    if ( !text.monitoredValueChanged() ) {
+                        text.setValue(f.valueAsString(),false);
                     }
                 }
             }
-
+            
         });
 
     }
-
-    public static Widget createReadOnlyWidgetFor(Field f, int height) {
-        if (useAutoResizeTextWidget(f)) {
+    
+    public static Widget createReadOnlyWidgetFor(Field f,int height) {
+        if ( useAutoResizeTextWidget( f ) ) {
             return createAutoResizeReadOnlyWidgetFor(f, height);
         }
-
+        
         return createDefaultReadOnlyWidgetFor(f, height);
     }
 
-    private static Widget createDefaultReadOnlyWidgetFor(Field f, int height) {
-        return createAutoResizeReadOnlyWidgetFor(f, height);
-    }
-
-    private static Widget createAutoResizeReadOnlyWidgetFor(Field f, int height) {
-        TextArea ta = new TextArea() {
-            protected com.google.gwt.user.client.ui.TextArea textBox() {
-                com.google.gwt.user.client.ui.TextArea ta = super.textBox();
-                ta.getElement().setAttribute("readonly", "readonly");
-                ta.getElement().getStyle().setProperty("fontSize", "11px");
-                ta.getElement().getStyle().setProperty("fontFamily", "Arial Unicode MS,Arial,sans-serif");
-                return ta;
-            }
-        };
-
-        if (f.value() != null) {
+    private static Widget createDefaultReadOnlyWidgetFor(Field f,int height) {
+        TextArea ta = new TextArea();
+        if ( f.value() != null ) {
             ta.setValue(f.valueAsString());
         }
 
         ta.setReadOnly(true);
-        FieldRenderOptions fro = f.renderOptions();
-        if (fro.width() != null) {
-            ta.setPreferredWidth(fro.width());
-        } else {
-            ta.setWidth100();
-        }
-        if (fro.height() != null) {
-            ta.setPreferredHeight(fro.height());
-        } else {
-            ta.setHeight(height);
-        }
 
+        ta.setWidth(200);
+        ta.setHeight(height);
+        
         FormItemStyle.applyReadOnlyTo(ta);
+        
+        return ta;
+    }
 
+    private static Widget createAutoResizeReadOnlyWidgetFor(Field f,int height) {
+        TextArea ta = new TextArea();
+        if ( f.value() != null ) {
+            ta.setValue(f.valueAsString());
+        }
+
+        ta.setReadOnly(true);
+
+        ta.setWidth(200);
+        ta.setHeight(height);
+        
+        FormItemStyle.applyReadOnlyTo(ta);
+        
         return ta;
     }
 }
